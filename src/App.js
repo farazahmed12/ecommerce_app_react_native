4bef21646d70cf931bf1dc14e4742ad574b64734
farazahmed <masterdot707@gmail.com>
2023-05-21 01:04:44 +0500

    completed product card section

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import {View, Text, StyleSheet, StatusBar, Switch} from 'react-native';
 import {useColorScheme} from 'nativewind';
-import React,{useEffect, useState} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchproducts } from './store/productsSlice.js';
-import { Provider } from "react-redux";
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchproducts} from './store/productsSlice.js';
+import {Provider} from 'react-redux';
 import store from './store/store.js';
+import Toast from 'react-native-toast-message';
 
 import ProductsList from './components/ProductsList.js';
 
 const App = () => {
-    
   const {colorScheme, toggleColorScheme} = useColorScheme();
 
-  
   return (
     <View className="flex-1 bg-gray-200 dark:bg-black">
-        <Switch value={colorScheme === 'dark' } onChange={toggleColorScheme} />
-      <Text className="text-red-300 text-4xl">New Collection</Text>
+      <View className='flex-row justify-between'>
+        <Text className="text-gray-700 text-2xl font-bold ml-2 dark:text-white/70">ShopStop</Text>
+        <Switch value={colorScheme === 'dark'} onChange={toggleColorScheme} />
+      </View>
 
       <ProductsList />
+      <Toast />
     </View>
   );
 };
 
 export default () => {
-    return (
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-  };
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,23 +1,79 @@
-import { View, Text , Image } from 'react-native'
-import React from 'react'
+import {View, Text, Image, TouchableOpacity, Modal} from 'react-native';
+import React, {useState} from 'react';
 import {useColorScheme} from 'nativewind';
+import Toast from 'react-native-toast-message';
+import {useDispatch, useSelector} from 'react-redux';
+
 
 const ProductCard = ({title, price, description, category, image}) => {
-    console.log('image =>>>', image);
+  const {colorScheme} = useColorScheme();
+  const [qty, setQty] = useState(1);
 
-    const {colorScheme} = useColorScheme()
+  const STATUS = useSelector(state => state.products.status)
+
+  console.log('status ===>',STATUS)
+
+  // Add to cart
+  const handleAddProduct = () => {
+   Toast.show({
+    type: 'success',
+    text1: 'Product added to cart'
+   })
+  };
   return (
-    <View className='w-full bg-white dark:bg-gray-50/10 rounded-3xl p-5 my-5'> 
+    <View className="w-full bg-white dark:bg-[#36454F] rounded-3xl p-5 my-5">
+    
 
-    <View className='bg-white rounded-xl'>
-        <Image source={{ uri: image}} className='w-full h-72' resizeMode='contain' />
+      <View className="bg-white rounded-xl">
+        <Image
+          source={{uri: image}}
+          className="w-full h-44"
+          resizeMode="contain"
+        />
+      </View>
+      <View className="mt-5">
+        <Text className="text-sm text-black/60 dark:text-white/70">
+          {category}
+        </Text>
+        <Text className="font-bold text-lg dark:text-white mb-4 ">{title}</Text>
+        <View className="flex-row mb-4 items-center gap-4">
+          <TouchableOpacity
+            onPress={() => {
+              if (qty === 1) {
+                setQty(1);
+              } else {
+                setQty(qty - 1);
+              }
+            }}>
+            <Image
+              source={require('../assets/minus.png')}
+              className="w-6 h-6"
+            />
+          </TouchableOpacity>
+          <Text className="text-xl dark:text-white">{qty}</Text>
+          <TouchableOpacity onPress={() => setQty(qty + 1)}>
+            <Image source={require('../assets/plus.png')} className="w-6 h-6" />
+          </TouchableOpacity>
+        </View>
+        <Text className="text-2xl font-extrabold dark:text-white">
+          ${(price * qty).toFixed(2)}
+        </Text>
+        <Text
+          numberOfLines={2}
+          className="text-sm text-black/60 dark:text-white/70">
+          {description}
+        </Text>
+
+        <TouchableOpacity
+          onPress={handleAddProduct}
+          className="flex-row justify-center w-8/12 self-center rounded-full p-3 bg-black dark:bg-white my-5">
+          <Text className="text-white font-bold dark:text-black">
+            Add to Cart
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
-      <Text>{title}</Text>
-      <Text>{description}</Text>
-      <Text>{price}</Text>
+  );
+};
 
-    </View>
-  )
-}
-
-export default ProductCard
+export default ProductCard;
