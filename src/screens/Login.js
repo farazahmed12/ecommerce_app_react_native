35bfeeee5e2995c6628c339e2f0601222ac74e45
farazahmed <masterdot707@gmail.com>
2023-05-31 23:13:56 +0500

    Added NetInfo for wifi checking

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,13 +7,32 @@ import {
   SafeAreaView,
   TextInput,
 } from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import NetInfo from '@react-native-community/netinfo';
 
 const Login = ({navigation}) => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [isInternet, setisInternet] = useState([]);
+  const [email, setemail] = useState('');
+  const [password, setpassword] = useState('');
+
+  console.log('Conn2 ===>', isInternet);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected);
+      setisInternet(state.isInternetReachable);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <SafeAreaView className="flex-1 ">
-      <View className="flex-1 bg-red-300">
-        <View className=" self-center mt-16 mb-3">
+    <View className="flex-1 bg-red-300">
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View className="flex-1 self-center mt-16 mb-3">
           <Image
             source={require('../assets/buy-now.png')}
             className="w-36 h-36"
@@ -26,11 +45,15 @@ const Login = ({navigation}) => {
           <TextInput
             placeholder="Email"
             className="border rounded  w-full p-2 my-4"
+            value={email}
+            onChange={e => setemail(e.target.value)}
           />
           <TextInput
             placeholder="Password"
             className="border rounded  w-full p-2 my-4"
             secureTextEntry={true}
+            value={password}
+            onChange={e => setpassword(e.target.value)}
           />
           <TouchableOpacity
             activeOpacity={0.7}
@@ -41,8 +64,15 @@ const Login = ({navigation}) => {
             </Text>
           </TouchableOpacity>
         </View>
-      </View>
-    </SafeAreaView>
+        {!isConnected && (
+          <View className=" absolute bottom-0 w-full bg-green-400">
+            <Text className="py-1 text-white text-center">
+              No Internet Access
+            </Text>
+          </View>
+        )}
+      </ScrollView>
+    </View>
   );
 };
 
